21ae65357271553499a134eb13e9130a9a6c6043
Thiago Sousa <thiagosousa81@gmail.com>
2023-04-17 22:51:15 -0300

    Create index.php

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Fenix-Line | Somos a inovação!</title>
+    <link type="text/css" rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <h1>Em construção...</h1>
+  </body>
+</html>
